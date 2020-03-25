225b0a2d4c5b3d6da078f7d27efcca460662ff5d
@brus2099 <indiebrus@gmail.com>
2020-03-24 23:06:38 -0600

    Creacion de componente basico y exportado de datos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const user1 = {
+  name : 'Brus', 
+  age : 21,
+  country : 'Mexico'
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function getInfo (user) {
+  return `My name is ${user.name} and I from ${user.country}. I have ${user.age} years old.`
+}
+
+const App = <h1> {getInfo(user1)} <br></br> {getInfo(user1)} </h1> 
+
+ReactDOM.render(App, document.getElementById('root'))
