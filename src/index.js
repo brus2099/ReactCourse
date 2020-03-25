7d7d49578fdd0978a0627aa4e953187e3671fb6d
@brus2099 <indiebrus@gmail.com>
2020-03-25 00:51:49 -0600

    Componentes con clases, estado de un componente, props y metodos dentro de un componente

--- src/index.js
+++ src/index.js
@@ -2,16 +2,95 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const user1 = {
-  name : 'Brus', 
-  age : 21,
-  country : 'Mexico'
+  name: 'Brus',
+  age: 21,
+  country: 'Mexico'
 }
 
 
-function getInfo (user) {
+function getInfo(user) {
   return `My name is ${user.name} and I from ${user.country}. I have ${user.age} years old.`
 }
 
-const App = <h1> {getInfo(user1)} <br></br> {getInfo(user1)} </h1> 
+class TarjetaFruta extends React.Component {
 
-ReactDOM.render(App, document.getElementById('root'))
+  constructor() {
+    super()
+
+    const METHODS =[
+      'agregar', 
+      'quitar',
+      'limpiar'
+    ]
+
+    METHODS.forEach((method) => {
+      this[method] = this[method].bind(this)
+    })
+
+    this.state = {
+      cantidad: 0
+    }
+  }
+
+  agregar () {
+    this.setState({
+      cantidad: this.state.cantidad + 1
+    })
+  }
+
+  quitar () {
+    this.setState({
+      cantidad: this.state.cantidad - 1
+    })
+  }
+
+  limpiar () {
+    this.setState({
+      cantidad: 0
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.name}</h2>
+        <div>Cantidad: {this.state.cantidad}</div>
+        <button
+          /* El siguiente es un ejemplo de mala programación, debido a que afecta a todo el estadp
+           * y los componentes deben renderizarse nuevamente cada uno. Se observa que el 
+           * método setState asigna un nuevo valor numerico al estado, pero este estado está asociado a
+           * cada componente que lo está usando. 
+           
+          
+          onClick={() => {
+            //this.state.cantidad = 100
+            //this.forceUpdate()
+            this.setState({ 
+              cantidad: this.state.cantidad + 1
+             })
+          }} */
+
+          // Una manera adecuada es la siguiente:
+          onClick={this.agregar}
+        >
+          +
+        </button>
+        <button onClick={this.quitar}>-</button>
+        <button onClick={this.limpiar}>Limpiar</button>
+        <hr />
+        <p>${this.props.price}</p>
+      </div>
+    )
+  }
+}
+
+const App = () => (
+  <div>
+    <TarjetaFruta name={'Sandia'} price={2.00} />
+    <TarjetaFruta name='Uva' price={1.5} />
+    <TarjetaFruta name='Kiwi' price={3.00} />
+    <TarjetaFruta name='Manzana' price={5.50} />
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
